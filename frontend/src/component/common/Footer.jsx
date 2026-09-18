34ba1131334bcf87a94a8a20e4c79a1bd5366bde
sayan <metyasayan3@gmail.com>
2026-09-18 17:08:56 +0530

    feat(frontend): add common components (Navbar, Footer, Pagination, RoomResult, RoomSearch)

---
+++ frontend/src/component/common/Footer.jsx
@@ -0,0 +1,13 @@
+const FooterComponent = () => {
+
+
+    return (
+        <footer>
+            <span className="my-footer">
+                Hotelix | All Rights Reserved &copy; {new Date().getFullYear()}
+            </span>
+        </footer>
+    );
+};
+
+export default FooterComponent;
