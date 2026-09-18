17bdce27ef54ef95b41e22f56c7a95fbcf127094
sayan <metyasayan3@gmail.com>
2026-09-18 17:07:09 +0530

    feat(backend): add utils (JWTUtils, Utils), HotelixApplication main class, and tests

---
+++ backend/src/main/java/com/hotelix/HotelixApplication.java
@@ -0,0 +1,13 @@
+package com.hotelix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelixApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/hotelix/HotelixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
